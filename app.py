413c555771569eb9522c11b10a9aab82805e8d40
jirapon46 <51775040+jirapon46@users.noreply.github.com>
2019-08-15 12:59:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-    if (userText == 'ไปเถอะ') :
-        sendText(user,'ไปไหนอ่ะ')
+    if (userText == 'สวัสดี') :
+        sendText(user,'เออดีด้วย')
     elif (userText == 'ไปนะ') :
-        sendText(user,'ชวนจริ๊งงง')
+        sendText(user,'จะไปตายที่ไหนก็ไป')
     else :
         sendText(user,'ว่าไงนะ')
     return '',200
